154123bce2fb3def3b097fb1f5f5ceee36ca5d58
frost8111981 <73012443+frost8111981@users.noreply.github.com>
2021-03-08 16:44:58 +0300

    Initial commit

---
+++ src/com/frost/Main.java
@@ -0,0 +1,28 @@
+package com.frost;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+public class Main {
+    public static List getListForGet() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        return arrayList;
+    }
+
+    public static List getListForSet() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        return arrayList;
+    }
+
+    public static List getListForAddOrInsert() {
+        LinkedList<String> linkedList = new LinkedList<>();
+        return linkedList;
+    }
+
+    public static List getListForRemove() {
+        LinkedList<String> linkedList = new LinkedList<>();
+        return linkedList;
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
